44ee62b0085642dda558b98180e48fe5b0c24114
Tsebek <badmaev.t@gmail.com>
2023-12-16 21:28:17 -0800

    Initial commit

---
+++ postgres/sql-scripts/init.sql
@@ -0,0 +1,11 @@
+create database if not exists raw;
+
+create schema if not exists raw.brazil_ecomm;
+
+-- create database if not exists dbt_dev;
+
+-- create schema if not exists dbt_dev.brazil_ecomm;
+
+-- create database if not exists dbt_prod;
+
+-- create schema if not exists dbt_prod.brazil_ecomm;
